ec95d51e680b168ff2bb5354590a7778e7a4bc67
Axelsson Isak (2) (Consultant) <isak.axelsson.2@consultant.volvo.com>
2024-07-09 06:47:22 +0200

    added function text

--- backend/src/database.py
+++ backend/src/database.py
@@ -2,115 +2,126 @@ from flask import jsonify
 from flask_socketio import SocketIO
 from pymongo import ReturnDocument
 from pymongo.mongo_client import MongoClient
-import os 
+import os
 from dotenv import load_dotenv, dotenv_values
 
-load_dotenv('/home/isak/projects/Sigrid/backend/src/variable.env')
+load_dotenv("/home/isak/projects/Sigrid/backend/src/variable.env")
 connectionString = os.getenv("MONGO_STRING")
 client = MongoClient(connectionString)
 
-DATABASE_NAME = 'SmartHome'
-COLLECTION_SHOPPINGITEMS = 'ShoppingItems'
-COLLECTION_ALARM = 'Alarms'
-COLLECTION_USER = 'User'
+DATABASE_NAME = "SmartHome"
+COLLECTION_SHOPPINGITEMS = "ShoppingItems"
+COLLECTION_ALARM = "Alarms"
+COLLECTION_USER = "User"
+
 
 def login(username, password):
+    """Function checks if user exist or not and if password matches"""
     try:
         collection = client.get_database(DATABASE_NAME).get_collection(COLLECTION_USER)
-        user = collection.find_one({'Username' : username})
-        if user['password'] == password:
-            return 'OK'
-     
+        user = collection.find_one({"Username": username})
+        if user["password"] == password:
+            return "OK"
+
     except Exception as e:
         return e
-    
-    return jsonify({"Bad request": "Failed login"})  
-    
+
+    return jsonify({"Bad request": "Failed login"})
+
+
 def get_all_alarm():
+    """Function gets all alarms from database"""
     try:
         collection = client.get_database(DATABASE_NAME).get_collection(COLLECTION_ALARM)
-        alarms = collection.find({}, {'_id': 0})
+        alarms = collection.find({}, {"_id": 0})
         return list(alarms)
     except Exception as e:
-        return 'NOT OK'
+        return "NOT OK"
+
 
 def create_alarm(hour, minute):
+    """Create a alarm in database"""
     try:
         collection = client.get_database(DATABASE_NAME).get_collection(COLLECTION_ALARM)
-        collection.insert_one({'hour': hour, 'minute': minute, 'active': True})
+        collection.insert_one({"hour": hour, "minute": minute, "active": True})
         return "OK"
     except Exception as e:
         return e
 
+
 def find_alarm(hour, minute, state):
+    """Find the requested alarm"""
     try:
         print(type(hour), type(minute), type(state))
         collection = client.get_database(DATABASE_NAME).get_collection(COLLECTION_ALARM)
-        update = {'$set': {'active':state }}
+        update = {"$set": {"active": state}}
         alarm = collection.find_one_and_update(
-            {'hour': hour, 'minute': minute},
-              update=update,
-              projection={'_id':0, 'active':0},
-              return_document=ReturnDocument.AFTER)
-              
-        #check if alarm exist
-        if alarm == None:
-            return 'NOT OK'
-        
+            {"hour": hour, "minute": minute},
+            update=update,
+            projection={"_id": 0, "active": 0},
+            return_document=ReturnDocument.AFTER,
+        )
+
+        # check if alarm exist
+        if alarm is None:
+            return "NOT OK"
+
     except Exception as e:
-        return 'NOT OK'
-    
-    return 'OK'
+        return "NOT OK"
+
+    return "OK"
+
 
 def get_active_alarms():
-    try:    
+    """Get all alarm items from database and sends it back"""
+    try:
         collection = client.get_database(DATABASE_NAME).get_collection(COLLECTION_ALARM)
-        alarm = collection.find_one({'active': True})
-        print(alarm['active'])
+        alarm = collection.find_one({"active": True})
+        print(alarm["active"])
         if alarm == None:
-            return 'NOT OK'
+            return "NOT OK"
 
-        return alarm.get('active')
+        return alarm.get("active")
     except Exception as e:
-        return 'NOT OK'
+        return "NOT OK"
+
 
 def fetch_shopping_items():
+    """Gets all the shopping items and sends it back"""
     try:
-        collection = client.get_database(DATABASE_NAME).get_collection(COLLECTION_SHOPPINGITEMS)
-        shoppinglist = collection.find({}, {'_id': 0})
+        collection = client.get_database(DATABASE_NAME).get_collection(
+            COLLECTION_SHOPPINGITEMS
+        )
+        shoppinglist = collection.find({}, {"_id": 0})
         return list(shoppinglist)
-    
+
     except Exception as e:
-        return 'NOT OK'
+        return "NOT OK"
+
 
 def create_shopping_item(itemName):
+    """Creates a shopping item and sends it to the database"""
     try:
-        collection = client.get_database(DATABASE_NAME).get_collection(COLLECTION_SHOPPINGITEMS)
-        _ = collection.insert_one({'shoppingItem': itemName, 'checked': False})
-        return 'OK'
-    
+        collection = client.get_database(DATABASE_NAME).get_collection(
+            COLLECTION_SHOPPINGITEMS
+        )
+        _ = collection.insert_one({"shoppingItem": itemName, "checked": False})
+        return "OK"
+
     except Exception as e:
-        return 'NOT OK'
-    
+        return "NOT OK"
+
+
 def delete_shopping_item(itemName):
+    """Deletes a specific shopping item"""
     try:
-        collection = client.get_database(DATABASE_NAME).get_collection(COLLECTION_SHOPPINGITEMS)
-        result = collection.delete_one({'shoppingItem': itemName})
+        collection = client.get_database(DATABASE_NAME).get_collection(
+            COLLECTION_SHOPPINGITEMS
+        )
+        result = collection.delete_one({"shoppingItem": itemName})
         if result.deleted_count == 1:
-            return 'OK'
-        else: 
-            return 'NOT OK'
+            return "OK"
+        else:
+            return "NOT OK"
     except Exception as e:
-        return 'NOT OK'
-
-def watch_shopping_items():
-    with client.get_database(DATABASE_NAME).get_collection(COLLECTION_SHOPPINGITEMS).watch(full_document='updateLookup') as stream:
-        for change in stream:
-            SocketIO.emit('shopping change', change)
-            print(change)
-
-def watch_alarm_items():
-    with client.get_database(DATABASE_NAME).get_collection(COLLECTION_ALARM).watch(full_document='updateLookup') as stream:
-        for change in stream:
-            SocketIO.emit('alarm change', change)
-            print(change)
+        return "NOT OK"
--- backend/src/main.py
+++ backend/src/main.py
@@ -9,149 +9,160 @@ import util.coffeeMachine
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app)
-state = "Off"
 thread_pool_manager = threadManager.ThreadManager()
 
-@app.route('/toggleAlarm', methods=['POST'])
+
+@app.route("/toggleAlarm", methods=["POST"])
 def toggle_alarm():
+    """REST-API - sets a alarm to active or inactive."""
     data = request.get_json()
     if not data:
-        return jsonify({'Error': 'Invalid input'}), 400
+        return jsonify({"Error": "Invalid input"}), 400
     try:
-        hour = int(data.get('hour'))
-        minute = int(data.get('minute'))
-        state = bool(data.get('active'))
+        hour = int(data.get("hour"))
+        minute = int(data.get("minute"))
+        state = bool(data.get("active"))
     except (KeyError, ValueError):
-        return jsonify({'Error': 'Invalid input'}), 400
-    
+        return jsonify({"Error": "Invalid input"}), 400
+
     status = db.findAlarm(hour, minute, state)
-    if status == 'NOT OK':
-        return jsonify({'Error': 'Invalid input'})
-    
+    if status == "NOT OK":
+        return jsonify({"Error": "Invalid input"})
+
     time = datetime.time(hour=hour, minute=minute)
 
     if state:
         thread_pool_manager.submit_task(time)
-        return jsonify({"Succes": 'Alarm has been turned on'}),200
-    
-    #If a active alarm is set to be deactived
-    thread_pool_manager.close_task(time)
-    return jsonify({"Succes": "Alarm has been turned off"}),200
+        return jsonify({"Succes": "Alarm has been turned on"}), 200
 
-@app.route('/toggleCoffee', methods=['POST'])
+    thread_pool_manager.close_task(time)
+    return jsonify({"Succes": "Alarm has been turned off"}), 200
+
+
+@app.route("/toggleCoffee", methods=["POST"])
 def toggle_coffe():
+    """Toogle on and off the coffe machine by sending a string with websocket to a micro controller."""
     data = request.get_json()
     print(data)
     if not data:
-        return jsonify({'Error': 'Invalid input'}), 400
+        return jsonify({"Error": "Invalid input"}), 400
     try:
-        state = str(data.get('state'))
-        #if state != ('On' or 'Off'):
-            #return jsonify({'Error:' 'Invalid input'}), 400
+        state = str(data.get("state"))
+        if state != ('On' or 'Off'):
+            return jsonify({'Error:' 'Invalid input'}), 400
     except (KeyError, ValueError):
-        return jsonify({'Error': 'Inavlid input'}), 400
-    
+        return jsonify({"Error": "Inavlid input"}), 400
+
     result = util.coffeeMachine.setCoffe(state)
     if not result:
-        return jsonify({'Error': 'Internal server error'}), 500
-    return jsonify({'Succes': 'Message sent to arduino'}), 200
+        return jsonify({"Error": "Internal server error"}), 500
+    return jsonify({"Succes": "Message sent to arduino"}), 200
 
 
-@app.route('/createAlarm', methods=['POST'])
+@app.route("/createAlarm", methods=["POST"])
 def create_alarm():
+    """Creates a alarm in the server and sets it to active"""
     data = request.get_json()
     if not data:
-        return jsonify({'Error': 'Invalid input'}), 400
-    
+        return jsonify({"Error": "Invalid input"}), 400
+
     try:
-        hour = int(data.get('hour'))
-        minute = int(data.get('minute')) 
-    except(KeyError, ValueError):
-        return jsonify({'Error': 'Invalid input'}), 400
-    
+        hour = int(data.get("hour"))
+        minute = int(data.get("minute"))
+    except (KeyError, ValueError):
+        return jsonify({"Error": "Invalid input"}), 400
+
     status = db.create_alarm(hour, minute)
     if status != "OK":
-        return jsonify({'Error': status}), 500
-    
+        return jsonify({"Error": status}), 500
+
+    # should be a try catch here
     time = datetime.time(hour=hour, minute=minute)
     thread_pool_manager.submit_task(time)
-    
+
     return jsonify({"Succes": "Alarm has been created"}), 200
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
+    """Login infunction, checks if the correct type of data is sent or not"""
     data = request.get_json()
     if not data:
-        return jsonify({'Error': 'Invalid input'})
-    
+        return jsonify({"Error": "Invalid input"})
+
     try:
-        username = str(data.get('username', 0))
-        password = str(data.get('password', 0))
-    except(KeyError, ValueError):
-        return jsonify({'Error': 'Invalid data'}), 400
-    
+        username = str(data.get("username", 0))
+        password = str(data.get("password", 0))
+    except (KeyError, ValueError):
+        return jsonify({"Error": "Invalid data"}), 400
+
     status = db.login(username, password)
-    if status !='OK':
-        return jsonify({'Error': 'Wrong password or username'}), 500
+    if status != "OK":
+        return jsonify({"Error": "Wrong password or username"}), 500
 
-    return jsonify({'Succes': 'Login succesful'}), 200
+    return jsonify({"Succes": "Login succesful"}), 200
 
-@app.route('/getAllAlarms', methods=['GET'])
+@app.route("/getAllAlarms", methods=["GET"])
 def get_all_alarms():
+    """Fetches all alarms in database"""
     alarms = db.get_all_alarm()
-    if alarms == 'NOT OK':
+    if alarms == "NOT OK":
         return jsonify({"Error": "Invalid Input"}), 500
-    
+
     return jsonify({"Alarms": alarms}), 200
 
-@app.route('/checkActive', methods=['GET'])
+
+@app.route("/checkActive", methods=["GET"])
 def check_active():
-    active = db.get_active_alarms()   
-    if active == 'NOT OK':
-        return jsonify({'Error': 'Invalid input'}), 500
-    
-    return jsonify({'active': active}), 200
+    """Function checks if any coffe_alarms exist"""
+    active = db.get_active_alarms()
+    if active == "NOT OK":
+        return jsonify({"Error": "Invalid input"}), 500
+    return jsonify({"active": active}), 200
 
-@app.route('/getShoppingItems', methods=['GET'])
+@app.route("/getShoppingItems", methods=["GET"])
 def get_shopping_list():
-    shoppingList = db.fetch_shopping_items()
-    if shoppingList == 'NOT OK':
-        return jsonify({'Error': 'Internal server error'}), 500
-    
-    return jsonify({'Shopping items': shoppingList})
+    """Function - gets all of the shopping items"""
+    shopping_list = db.fetch_shopping_items()
+    if shopping_list == "NOT OK":
+        return jsonify({"Error": "Internal server error"}), 500
+    return jsonify({"Shopping items": shopping_list})
 
-@app.route('/createShoppingItems', methods=['POST'])
+
+@app.route("/createShoppingItems", methods=["POST"])
 def create_shopping_item():
+    """Function creates """
     data = request.get_json()
     print(data)
     if not data:
-        return jsonify({'Error': 'Invalid input'}), 400
-    
+        return jsonify({"Error": "Invalid input"}), 400
+
     try:
-        item = str(data['shoppingItem'])
+        item = str(data["shoppingItem"])
         item = item.capitalize()
         status = db.create_shopping_item(item)
-        if status == 'NOT OK':
-            return jsonify({'Error': 'Invalid input'}), 500
-        return jsonify({'Succes': 'Item added'}), 200
+        if status == "NOT OK":
+            return jsonify({"Error": "Invalid input"}), 500
+        return jsonify({"Succes": "Item added"}), 200
     
-    except(KeyError, ValueError):
-        return jsonify({'Error': 'Invalid input'}), 400
-    
-@app.route('/deteShoppingItems', methods=['delete'])
+    except (KeyError, ValueError):
+        return jsonify({"Error": "Invalid input"}), 400
+
+@app.route("/deteShoppingItems", methods=["DELETE"])
 def delete_shopping_items():
+    """Function finds the shopping item and deletes it from data base"""
     data = request.get_json()
     print(data)
     for item in data:
-        status = db.delete_shopping_item(item['shoppingItem'])
-        if status == 'NOT OK':
-            return jsonify({'Error': 'Internal server error'}), 500
-    socketio.emit('shopping')
-    return jsonify({'Succes': 'items has been deleted'}), 200
+        status = db.delete_shopping_item(item["shoppingItem"])
+        if status == "NOT OK":
+            return jsonify({"Error": "Internal server error"}), 500
+    socketio.emit("shopping")
+    return jsonify({"Succes": "items has been deleted"}), 200
 
-    
 def run_flask():
-    Flask.run(app, host='0.0.0.0', port=5001)
+    """Function runs app"""
+    Flask.run(app, host="0.0.0.0", port=5001)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_flask()
